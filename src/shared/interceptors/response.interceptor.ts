317dbb71fd8e349463b0ebb7f30be1ee9ca4d315
Abraham15rivas <abraham15rivas@gmail.com>
2025-01-14 20:52:35 -0400

    fix return type StreamableFile

--- src/shared/interceptors/response.interceptor.ts
+++ src/shared/interceptors/response.interceptor.ts
@@ -2,15 +2,21 @@ import { Injectable, NestInterceptor, ExecutionContext, CallHandler } from '@nes
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { ResponseDto } from './response.dto';
+import { StreamableFile } from '@nestjs/common';
 
 @Injectable()
 export class ApiResponseInterceptor implements NestInterceptor {
-  intercept(context: ExecutionContext, next: CallHandler): Observable<ResponseDto<any>> {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     return next.handle().pipe(
       map(data => {
         if (data instanceof ResponseDto) {
           return data;
         }
+        // Si la respuesta es un StreamableFile, devuélvelo directamente
+        if (data instanceof StreamableFile) {
+          return data;
+        }
+
         return new ResponseDto({
           data,
           isValid: true,
